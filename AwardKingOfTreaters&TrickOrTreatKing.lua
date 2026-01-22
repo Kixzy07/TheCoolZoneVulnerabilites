4398b9ab05627cf1775a1a768a4c4b786c7f585c
bubbahaynes3308 <bubbahaynes3307@outlook.com>
2026-01-22 14:47:10 -0500

    z
    z

--- AwardKingOfTreaters&TrickOrTreatKing.lua
+++ AwardKingOfTreaters&TrickOrTreatKing.lua
@@ -1,4 +1,4 @@
---You'll need to have the shop UI Open and on your screen before Executing this
+--You'll need to Be on the Map Trickersville have the shop UI Open and on your screen before Executing this
 local args = {
     [1] = game:GetService("Players").LocalPlayer.PlayerGui.ShopUI,
     [2] = workspace.Map.Map.Shop.Spooky:FindFirstChild("King of the Treaters")
